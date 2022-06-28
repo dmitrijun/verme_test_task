7f5d8504c2d9f8cc5a0d096e70957e444dd078fd
Dmitry Malkov <domalkov@edu.hse.ru>
2022-06-29 01:37:19 +0300

    Added handler to get subtree

--- tree_serializer/views.py
+++ tree_serializer/views.py
@@ -1,19 +1,22 @@
 import json
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Tree
 # Create your views here.
+    
 
-
-def index(request):
+def get_tree(request):
     """
         Returns tree node list
     """
     records = Tree.objects.all()
-    serializable_records = [{
-        "id": item.pk,
-        "name": item.name,
-        "parent": item.parent_pk
-    } for item in records]
+    serializable_records = [item.to_dict() for item in records]
 
     return HttpResponse(serializable_records)
+
+def get_subtree(request, node_pk):
+    """
+        Returns Tree node by key
+    """
+    node = get_object_or_404(Tree, pk=node_pk)
+    return HttpResponse(f'{node.to_dict()}')
