6faa342a2d08785b0cb83da6a4b03e7c18bd97b3
Dmitry Malkov <domalkov@edu.hse.ru>
2022-06-29 00:01:31 +0300

    Added tree serializer to apps list

--- server/settings.py
+++ server/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'tree_serializer.apps.TreeSerializerConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
