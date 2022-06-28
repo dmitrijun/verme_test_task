54b95aba9d9b8cca0f169670fe8927f7937d5f14
Dmitry Malkov <domalkov@edu.hse.ru>
2022-06-29 01:36:36 +0300

    Fixed url path to accomodate nested pathes

--- server/urls.py
+++ server/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('tree_serializer', include("tree_serializer.urls"))
+    path('tree_serializer/', include("tree_serializer.urls"))
 ]
