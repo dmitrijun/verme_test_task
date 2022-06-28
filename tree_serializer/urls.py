6a68ae258b107fb7d710185d7d8192c1082df03e
Dmitry Malkov <domalkov@edu.hse.ru>
2022-06-29 01:37:03 +0300

    Added url to get subtree by node ID

--- tree_serializer/urls.py
+++ tree_serializer/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.get_tree, name='get_tree'),
+    path("<int:node_pk>/", views.get_subtree, name='get_subtree')
 ]
