f41d117e760719e8441e0841f30f895227fc5829
Dmitry Malkov <domalkov@edu.hse.ru>
2022-06-29 00:44:10 +0300

    Added parent_pk property

--- tree_serializer/models.py
+++ tree_serializer/models.py
@@ -7,3 +7,13 @@ class Tree(models.Model):
     parent = models.ForeignKey('self', models.CASCADE, null=True, blank=True,
                                verbose_name='Родитель')
     name = models.CharField(max_length=255, verbose_name='Название')
+
+    def __str__(self) -> str:
+        return f"{self.pk} - {self.name} - {self.parent_pk}"
+
+    @property
+    def parent_pk(self):
+        if self.parent:
+            return self.parent.pk
+        
+        return None
