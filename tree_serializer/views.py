65d73fb22e9016c0d230ddd35988b24441df1b8b
Dmitry Malkov <domalkov@edu.hse.ru>
2022-06-29 00:44:42 +0300

    Added View to represent all nodes of tree

--- tree_serializer/views.py
+++ tree_serializer/views.py
@@ -1,8 +1,19 @@
+import json
 from django.http import HttpResponse
 from django.shortcuts import render
-
+from .models import Tree
 # Create your views here.
+
+
 def index(request):
     """
-        Returns complete tree 
+        Returns tree node list
     """
+    records = Tree.objects.all()
+    serializable_records = [{
+        "id": item.pk,
+        "name": item.name,
+        "parent": item.parent_pk
+    } for item in records]
+
+    return HttpResponse(serializable_records)
