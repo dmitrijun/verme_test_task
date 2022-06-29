70681e7ff584f840e05811e08083e0a0f6589086
Dmitry Malkov <domalkov@edu.hse.ru>
2022-06-29 16:09:01 +0300

    Added subtree serialization

--- tree_serializer/views.py
+++ tree_serializer/views.py
@@ -1,9 +1,18 @@
 import json
+from re import T
 from django.http import HttpResponse
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import get_list_or_404, render, get_object_or_404
 from .models import Tree
 # Create your views here.
-    
+
+
+def bypass_tree(node_pk: int):
+    current_node = get_object_or_404(Tree, pk=node_pk)
+    serialization = current_node.to_short_dict()
+    serialization["children"] = [bypass_tree(item.pk)
+                                 for item in Tree.objects.filter(parent=current_node)]
+    return serialization
+
 
 def get_tree(request):
     """
@@ -12,11 +21,10 @@ def get_tree(request):
     records = Tree.objects.all()
     serializable_records = [item.to_dict() for item in records]
 
-    return HttpResponse(serializable_records)
 
 def get_subtree(request, node_pk):
     """
         Returns Tree node by key
     """
-    node = get_object_or_404(Tree, pk=node_pk)
-    return HttpResponse(f'{node.to_dict()}')
+    subtree_serialization = bypass_tree(node_pk)
+    return HttpResponse(json.dumps(subtree_serialization), content_type="application/json")
